9876febadc060a1fb6db2f72e9aa7f801f91f18b
Andreas Hadjiantoni <andreashadjiantonis@gmail.com>
2025-07-06 17:33:51 +0100

    Update lsp-zero.lua

--- lua/plugin/lsp-zero.lua
+++ lua/plugin/lsp-zero.lua
@@ -25,5 +25,6 @@ vim.diagnostic.config({
 -- Show line diagnostics automatically in hover window
 vim.o.updatetime = 250
 vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+vim.diagnostic.disable()
 
 return M
